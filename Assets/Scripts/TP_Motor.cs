beb33fc2a35753d5ba6e2d27d5f0e735b866dc0f
lorenzo456 <lorenzo456_@hotmail.com>
2015-10-27 12:51:12 +0100

    Character controller
    Reticle toegevoegt en de speed van de camera sneller gemaakt

---
+++ Assets/Scripts/TP_Motor.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TP_Motor : MonoBehaviour {
+
+    public static TP_Motor Instance;
+
+    public float MoveSpeed = 10f;
+    public Vector3 MoveVector { get; set; }
+
+	void Awake () {
+        Instance = this;
+	}
+	
+	public void UpdateMotor () {
+        SnapAlignCharacterWithCamera();
+        ProcessMotion();
+    }
+
+    void ProcessMotion(){
+        //Transform MoveVector into world space
+        MoveVector = transform.TransformDirection(MoveVector);
+        //normalize MoveVector if Magnitude >1
+        if(MoveVector.magnitude > 1)
+        {
+            MoveVector = Vector3.Normalize(MoveVector);
+        }
+        //Multiply MoveVector by MoveSpeed
+        MoveVector *= MoveSpeed;
+
+        //Multiply MoveVector by deltatime
+        MoveVector *= Time.deltaTime;
+
+        //Move Character in world space
+        TP_Controller.CharacterController.Move(MoveVector);
+    }
+
+    void SnapAlignCharacterWithCamera(){
+        if(MoveVector.x != 0 || MoveVector.z != 0)
+        {
+            transform.rotation = Quaternion.Euler(transform.eulerAngles.x, 
+                                                  Camera.main.transform.eulerAngles.y, 
+                                                  transform.eulerAngles.z);
+        }
+    }
+
+}
