e8d220f0a3bc510f9e9e0152f331e2302b6eee89
MrImitate <frank.toonen1@gmail.com>
2015-11-29 12:57:11 +0100

    modified:   Assets/Scripts/Gun scripts/Gun_Terraformer.cs

--- Assets/Scripts/Gun scripts/Gun_Terraformer.cs
+++ Assets/Scripts/Gun scripts/Gun_Terraformer.cs
@@ -77,7 +77,7 @@ public class Gun_Terraformer : Gun
                     Vector3 launchDir = Vector3.Normalize(objectsHit [n].transform.position - point);
                     launchDir.y = 1;
                     objectsHit [n].GetComponent<Player_Force>().AddImpact(launchDir, 50 * chargedRadius);
-                } else if (objectsHit [n].tag == "PhysicsObject" || objectsHit [n].tag == "Flag")
+                } else if (objectsHit [n].tag == "PhysicsObject" || objectsHit [n].tag == "Flag" || objectsHit [n].tag == "Ball")
                 {
                     objectsHit [n].GetComponent<Rigidbody>().AddForce(Vector3.up * 1000 * chargedRadius);
                 }
