6f306e80612acb359a35f9882afc6f15bfa8940a
lorenzo456 <lorenzo456_@hotmail.com>
2015-12-03 21:05:39 +0100

    How to play Button

---
+++ Assets/howToPlaySlideShow.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class howToPlaySlideShow : MonoBehaviour {
+    public GameObject howToPlaySlides;
+
+	public void EnableSlides()
+    {
+        howToPlaySlides.SetActive(true);
+    }
+}
---
+++ Assets/onClickDissapear.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class onClickDissapear : MonoBehaviour {
+
+
+	void Update () {
+        if (Input.anyKey)
+        {
+            this.gameObject.SetActive(false);
+        }
+	}
+}
