871f4c68a18bef46b1956cc787a1aed1b4fa2cbe
lorenzo456 <lorenzo456_@hotmail.com>
2015-12-03 22:21:40 +0100

    Start menu weer terug!! slides zijn nu beter

--- Assets/onClickDissapear.cs
+++ Assets/onClickDissapear.cs
@@ -3,9 +3,24 @@ using System.Collections;
 
 public class onClickDissapear : MonoBehaviour {
 
+    public GameObject howToPulseImage;
+    public GameObject howToTerraformImage;
 
 	void Update () {
-        if (Input.anyKey)
+        if (Input.anyKeyDown && !Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (howToPulseImage.activeSelf == true)
+            {
+                howToPulseImage.SetActive(false);
+                howToTerraformImage.SetActive(true);
+            }
+            else
+            {
+                howToTerraformImage.SetActive(false);
+                howToPulseImage.SetActive(true);
+            }
+        }
+        else if(Input.GetKey(KeyCode.Escape))
         {
             this.gameObject.SetActive(false);
         }
